a643861cb19ed587922e3adf787b81107eb6f98f
DasWelch <35710104+DasWelch@users.noreply.github.com>
2021-01-19 16:45:06 -0500

    added some tester data and started to adjust structure of content

--- App.js
+++ App.js
@@ -1,8 +1,8 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet,  View } from "react-native";
+import { StyleSheet, View } from "react-native";
 import MyLayout from "./src/components/layout.component";
-import { Tile,Text } from "react-native-elements";
+import { Tile, Text } from "react-native-elements";
 
 export default function App() {
   return (
@@ -32,6 +32,22 @@ const styles = StyleSheet.create({
     paddingLeft: 10,
     paddingRight: 10,
     paddingTop: 5,
-    paddingBottom: 5
+    paddingBottom: 5,
   },
 });
+
+const testdata = {
+  Apendix_A: {
+    STUDENT_PILOT_ENDORSEMENTS: [
+      {
+        code: `A.4`,
+        title: `Pre-solo flight training`,
+        fAR: {
+          Code: 61.87,
+          subs: [c1, c2],
+        },
+        body: `I certify that [First name, MI, Last name] has received and logged pre-solo flight training for the maneuvers and procedures that are appropriate to the [make and model] aircraft. I have determined [he or she] has demonstrated satisfactory proficiency and safety on the maneuvers and procedures required by § 61.87 in this or similar make and model of aircraft to be flown.`,
+      },
+    ],
+  },
+};
